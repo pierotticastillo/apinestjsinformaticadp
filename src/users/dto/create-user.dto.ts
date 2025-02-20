69e5e048c9995a8bc8d3b1642f73e918e37ac553
Enrique Alejandro Pierotti Castillo <pierotticastillo@gmail.com>
2025-02-20 18:28:09 -0300

    modified:   src/users/dto/create-user.dto.ts

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,4 +1,18 @@
-//& DATA TRANSFER OBJECT
-// Es un objeto utilizado para encapsular datos, y transferir esos datos entre capas de la aplicación
-// Pueden incluir validaciones de los datos antes de ser procesados por la aplicación
-export class CreateUserDto {}
+import { IsBoolean, IsNotEmpty, IsOptional, IsString } from 'class-validator';
+
+// & DTO (Data Transfer Object)
+// Define cómo debe lucir el cuerpo de la solicitud al crear un usuario.
+
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty({ message: 'El nombre es requerido' })
+  firstName: string;
+
+  @IsString()
+  @IsNotEmpty({ message: 'El apellido es requerido' })
+  lastName: string;
+
+  @IsBoolean()
+  @IsOptional()
+  isActive: boolean;
+}
