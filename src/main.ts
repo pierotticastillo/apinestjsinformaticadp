4afc2633b8945f7894e7e7153f17c0906a7d294c
Enrique Alejandro Pierotti Castillo <pierotticastillo@gmail.com>
2025-02-20 18:28:05 -0300

    UPDATE: - Instalación de class-transformer y class-validator para validar y transformar los datos recibidos. - Configuración de Validation Pipe para operar con validaciones de datos. - Actualización de la ruta con versionado "v1". - Incoporación de la entidad User al módulo para operar con el ORM. - Creación de métodos de la api para:   # Encontrar todos los usuarios   # Encontrar un usuario específico.   # Crear un usuario   # Modificar un usuario.   # Remover un usuario (Eliminación física). - Configuración de DTO para la comprobación de datos recibidos

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // ❌ Remueve propiedades no definidas en el DTO
+      forbidNonWhitelisted: true, // 🚫 Lanza error si se envían propiedades no permitidas
+      transform: true, // 🔄 Transforma payloads al tipo esperado en el DTO
+    }),
+  );
   const configService = app.get(ConfigService);
   const port = configService.get<number>('PORT') || 3000;
 
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,26 +1,43 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+  ) {}
+  async create(createUserDto: CreateUserDto) {
+    return await this.usersRepository.save(createUserDto);
   }
 
-  findAll() {
-    return `This action returns all users`;
+  async findAll() {
+    return await this.usersRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async findOne(id: number) {
+    return await this.usersRepository.findOneBy({ id });
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  async update(id: number, updateUserDto: UpdateUserDto) {
+    const user = await this.usersRepository.findOneBy({ id });
+    if (!user) {
+      return `No se encontró un usuario con id ${id} o tal vez fue eliminado`;
+    }
+    await this.usersRepository.update(id, updateUserDto);
+    return await this.usersRepository.findOneBy({ id });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async remove(id: number) {
+    const user = await this.usersRepository.findOneBy({ id });
+    if (!user) {
+      return `No se encontró un usuario con id ${id} o tal vez fue eliminado`;
+    }
+    await this.usersRepository.delete(id);
+    return `El usuario con id ${id} ha sido eliminado de la base de datos `;
   }
 }
